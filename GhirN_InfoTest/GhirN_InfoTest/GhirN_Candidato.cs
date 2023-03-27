190515d288c32b5f376a7fc08eea43dfa1d99e99
nicoghiro <94957910+nicoghiro@users.noreply.github.com>
2023-03-27 08:28:59 +0200

    8.28
    8.28

---
+++ GhirN_InfoTest/GhirN_InfoTest/GhirN_Candidato.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhirN_InfoTest
+{
+    abstract class GhirN_Candidato
+    {
+        private int GhirN_matricola;
+        private string GhirN_nome;
+       
+     public int ghirN_matricola {
+           get
+            {
+                return GhirN_matricola;
+            }
+          private  set
+            {
+                ghirN_matricola = value;
+            }
+        }
+        public string ghirN_nome
+        {
+            get
+            {
+                return GhirN_nome;
+            }
+            private set
+            {
+                ghirN_nome = value;
+            }
+        }
+
+        public GhirN_Candidato()
+        {
+            GhirN_matricola = 0;
+            GhirN_nome = "";
+
+        }
+        public GhirN_Candidato(string Nome)
+        {
+            GhirN_matricola = 0;
+            GhirN_nome = Nome;
+
+        }
+        public GhirN_Candidato(int Matricola)
+        {
+            ghirN_matricola = Matricola;
+            ghirN_nome = "";
+
+        }
+          public GhirN_Candidato(string Nome,int Matricola)
+        {
+            ghirN_matricola = Matricola;
+            ghirN_nome = Nome;
+
+        }
+    }
+}
