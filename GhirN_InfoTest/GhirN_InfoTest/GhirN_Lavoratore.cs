d8ec3193effce20d2d067c3dd2912b0ae9f57ec2
nicoghiro <94957910+nicoghiro@users.noreply.github.com>
2023-03-27 09:31:15 +0200

    Task3
    9.31

---
+++ GhirN_InfoTest/GhirN_InfoTest/GhirN_Lavoratore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhirN_InfoTest
+{
+    public class GhirN_Lavoratore:GhirN_Candidato
+    {
+        int GhirN_esperienze;
+        public int ghirN_esperienze
+        {
+            get { return GhirN_esperienze; }
+            set { 
+                if(0<=value&&value<=5)
+                GhirN_esperienze = value;
+                else
+                {
+                    GhirN_esperienze = 0;
+                    throw new Exception("inserire un numero valido di esperienze 0 <= x <= 5 ");
+                }
+
+            }
+        }
+        public GhirN_Lavoratore() : base()
+        {
+            ghirN_esperienze = 0;
+        }
+        public GhirN_Lavoratore(int esperienze, int matricola ,string nome) : base(nome ,matricola)
+        {
+            ghirN_esperienze = esperienze;
+        }
+        public override int punteggio()
+        {
+            return ghirN_esperienze*20;
+        }
+        public override bool Isidoneo()
+        {
+            if (this.punteggio() >= 60)
+                return true;
+            else
+                return false;
+        }
+    }
+}
